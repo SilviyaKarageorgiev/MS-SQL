5ba7d6ea5c8083cfc2f0d21acb1fcfa74a3a6f2f
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-01-21 22:00:32 +0200

    Update 01.One-To-OneRelationship.sql

--- 03. Table Relations/01.One-To-OneRelationship.sql
+++ 03. Table Relations/01.One-To-OneRelationship.sql
@@ -112,3 +112,23 @@ INSERT INTO [StudentsExams]([StudentID], [ExamID])
 			(2, 102),
 			(2, 103)
 			
+
+-- Problem 04. Self-Referencing
+
+CREATE TABLE [Teachers]
+(
+	[TeacherID] INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] NVARCHAR(50) NOT NULL,
+	[ManagerID] INT FOREIGN KEY REFERENCES [Teachers]([TeacherID])
+)
+
+INSERT INTO [Teachers]([Name], [ManagerID])
+	 VALUES
+			('John', NULL),
+			('Maya', 106),
+			('Silvia', 106),
+			('Ted', 105),
+			('Mark', 101),
+			('Greta', 101)
+			
+

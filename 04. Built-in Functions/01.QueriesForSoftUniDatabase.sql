946f031e49d8d79394c0a8b42c260d233113cc33
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-01-23 00:14:29 +0200

    Update 01.QueriesForSoftUniDatabase.sql

--- 04. Built-in Functions/01.QueriesForSoftUniDatabase.sql
+++ 04. Built-in Functions/01.QueriesForSoftUniDatabase.sql
@@ -71,7 +71,7 @@ SELECT [FirstName], [LastName]
 -- Problem 10. Rank Employees by Salary
 
    SELECT [EmployeeID], [FirstName], [LastName], [Salary],
-		      DENSE_RANK() OVER(PARTITION BY [Salary] ORDER BY [EmployeeID])
+	  DENSE_RANK() OVER(PARTITION BY [Salary] ORDER BY [EmployeeID])
      FROM [Employees]
     WHERE [Salary] BETWEEN 10000 AND 50000
  ORDER BY [Salary] DESC

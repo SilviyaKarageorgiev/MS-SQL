2e2a8e43102d911b878766b5d93f407517d01213
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-01-24 17:27:54 +0200

    Update 03.QueriesForDiabloDatabase.sql

--- 04. Built-in Functions/03.QueriesForDiabloDatabase.sql
+++ 04. Built-in Functions/03.QueriesForDiabloDatabase.sql
@@ -23,3 +23,22 @@ FROM [Users]
 WHERE [IpAddress] LIKE '___.1_%._%.___'
 ORDER BY [Username]
 
+
+-- Problem 17. Show All Games with Duration & Part of the Day
+
+   SELECT [Name] AS [Game],
+     CASE
+          WHEN DATEPART(HOUR, [Start]) >= 0 AND DATEPART(HOUR, [Start]) < 12 THEN 'Morning'
+          WHEN DATEPART(HOUR, [Start]) >= 12 AND DATEPART(HOUR, [Start]) < 18 THEN 'Afternoon'
+          ELSE 'Evening'
+     END
+      AS [Part of the Day],
+    CASE 
+          WHEN [Duration] <= 3 THEN 'Extra Short'
+          WHEN [Duration] BETWEEN 4 AND 6 THEN 'Short'
+          WHEN [duration] > 6 THEN 'Long'
+          ELSE 'Extra Long'
+     END
+      AS [Duration]
+    FROM [Games]
+ORDER BY [Game], [Duration], [Part of the Day]

9cd3abaae941d3a82dea053fdf02934deeb826ca
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-01-24 16:12:22 +0200

    Update 02.QueriesForGeographyDatabase.sql

--- 04. Built-in Functions/02.QueriesForGeographyDatabase.sql
+++ 04. Built-in Functions/02.QueriesForGeographyDatabase.sql
@@ -3,7 +3,7 @@ USE [Geography]
 -- Problem 12. Countries Holding 'A' 3 or More Times
 
   SELECT [CountryName] AS [Country Name],
-  	     [IsoCode] AS [ISO Code]
+  	 [IsoCode] AS [ISO Code]
     FROM [Countries]
    WHERE [CountryName] LIKE '%a%a%a%'
 ORDER BY [IsoCode]
@@ -11,9 +11,11 @@ ORDER BY [IsoCode]
 
 -- Problem 13. Mix of Peak and River Names
 
-SELECT [PeakName],
-	   [RiverName],
-	   CONCAT(LOWER([PeakName]), LOWER(SUBSTRING([RiverName], 2, LEN([RiverName])))) AS [Mix]
-FROM [Peaks], [Rivers]
-WHERE RIGHT([PeakName], 1) = LEFT([RiverName], 1)
+  SELECT [PeakName],
+         [RiverName],
+         LOWER(CONCAT([PeakName], SUBSTRING([RiverName], 2, LEN([RiverName])))) 
+      AS [Mix]
+    FROM [Peaks], 
+         [Rivers]
+   WHERE RIGHT(LOWER([PeakName]), 1) = LEFT(LOWER([RiverName]), 1)
 ORDER BY [Mix]

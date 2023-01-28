f168fbe953abe71a8acbfb06d6544c47c2f40949
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-01-28 13:59:32 +0200

    Add

--- 05. Subqueries and Joins/01.QueriesForSoftUniDatabase.sql
+++ 05. Subqueries and Joins/01.QueriesForSoftUniDatabase.sql
@@ -39,3 +39,40 @@ ORDER BY [e].[FirstName], [e].[LastName]
 ORDER BY [e].[EmployeeID]
 
 
+-- Problem 04. Employee Departments
+
+  SELECT 
+		 TOP(5)
+		 [e].[EmployeeID],
+		 [e].[FirstName],
+		 [e].[Salary],
+		 [d].[Name] AS [DepartmentName]
+		 FROM [Employees] AS [e]
+    JOIN [Departments] AS [d] ON [e].[DepartmentID] = [d].[DepartmentID]
+   WHERE [e].[Salary] > 15000
+ORDER BY [e].[DepartmentID]
+
+
+-- Problem 05. Employees Without Projects
+
+   SELECT 
+          TOP(3) 
+          [e].[EmployeeID],
+		  [e].[FirstName]
+     FROM [Employees] AS [e]
+LEFT JOIN [EmployeesProjects] AS [ep] ON [e].[EmployeeID] = [ep].[EmployeeID]
+    WHERE [ep].[ProjectID] IS NULL
+ ORDER BY [e].[EmployeeID]
+
+
+-- Problem 06. Employees Hired After
+
+  SELECT [e].[FirstName], 
+         [e].[LastName], 
+		 [e].[HireDate], 
+		 [d].[Name] AS [DeptName] 
+    FROM [Employees] AS [e]
+    JOIN [Departments] AS [d] ON [e].[DepartmentID] = [d].[DepartmentID]
+   WHERE [e].[HireDate] > '1999-01-01' 
+     AND [d].[Name] IN ('Sales', 'Finance')
+ORDER BY [e].[HireDate]

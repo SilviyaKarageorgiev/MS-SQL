16479163ccb20a3c03f563514e30a1c4703bfbfd
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-01-28 16:07:00 +0200

    Update 01.QueriesForSoftUniDatabase.sql

--- 05. Subqueries and Joins/01.QueriesForSoftUniDatabase.sql
+++ 05. Subqueries and Joins/01.QueriesForSoftUniDatabase.sql
@@ -123,3 +123,30 @@ SELECT
    WHERE [e].[ManagerID] IN (3, 7)
 ORDER BY [e].[EmployeeID]
 
+
+-- Problem 10. Employees Summary
+
+  SELECT 
+         TOP(50)
+         [e].[EmployeeID],
+         CONCAT([e].[FirstName], ' ', [e].[LastName]) AS [EmployeeName],
+         CONCAT([m].[FirstName], ' ', [m].[LastName]) AS [ManagerName], 
+         [d].[Name] AS [DepartmentName]
+    FROM [Employees] AS [e]
+    JOIN [Employees] AS [m]
+      ON [e].[ManagerID] = [m].[EmployeeID]
+    JOIN [Departments] AS [d]
+      ON [e].[DepartmentID] = [d].[DepartmentID]
+ORDER BY [EmployeeID]
+
+
+-- Problem 11. Min Average Salary
+
+  SELECT MIN([AvgSalary]) AS [MinAverageSalary]
+    FROM
+   (
+  SELECT AVG([Salary]) AS [AvgSalary] 
+    FROM [Employees]
+GROUP BY [DepartmentID]
+    ) AS [AverageSalaries]
+    

a1a4b2cdee818b97b206e77429ce83f80ca89e8e
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-02-05 13:58:32 +0200

    Update 10-Aug-2022.sql

--- Exam Preparation/10-Aug-2022/10-Aug-2022.sql
+++ Exam Preparation/10-Aug-2022/10-Aug-2022.sql
@@ -156,3 +156,19 @@ LEFT JOIN [BonusPrizes] AS [b] ON [tb].[BonusPrizeId] = [b].[Id]
  ORDER BY [t].[Name] ASC
  
  
+-- Problem 10
+
+SELECT DISTINCT SUBSTRING([t].[Name], CHARINDEX(' ', [t].[Name]) + 1, LEN([t].[Name])) 
+      AS [LastName],
+         [t].[Nationality],
+         [t].[Age],
+         [t].[PhoneNumber]
+    FROM [Tourists] AS [t]
+    JOIN [SitesTourists] AS [st]
+      ON [t].[Id] = [st].[TouristId]
+    JOIN [Sites] AS [s]
+      ON [s].[Id] = [st].[SiteId]
+    JOIN [Categories] AS [c]
+      ON [c].[Id] = [s].[CategoryId]
+   WHERE [c].[Name] = 'History and archaeology'
+ORDER BY [LastName]

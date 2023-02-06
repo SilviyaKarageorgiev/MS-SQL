96e5002180eb80406b4558a60298e3582cac2742
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-02-07 00:04:00 +0200

    Add

--- Exam Preparation/19-Jun-2022/19-Jun-2022.sql
+++ Exam Preparation/19-Jun-2022/19-Jun-2022.sql
@@ -218,3 +218,24 @@ SELECT dbo.udf_GetVolunteersCountFromADepartment ('Zoo events')
 
 GO
 
+
+-- Problem 12
+
+CREATE PROCEDURE [usp_AnimalsWithOwnersOrNot] (@AnimalName VARCHAR(30))
+              AS
+           BEGIN
+                       SELECT [a].[Name],
+                              ISNULL([o].[Name], 'For adoption')
+                           AS [OwnersName]
+                         FROM [Animals]
+                           AS [a]
+                    LEFT JOIN [Owners]
+                           AS [o]
+                           ON [a].[OwnerId] = [o].[Id]
+                        WHERE [a].[Name] = @AnimalName
+             END
+ 
+GO
+
+EXEC usp_AnimalsWithOwnersOrNot 'Pumpkinseed Sunfish'
+EXEC usp_AnimalsWithOwnersOrNot 'Hippo'

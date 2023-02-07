dea292f5ce91116c4163343be75f03efa066c2e2
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-02-07 15:53:37 +0200

    Add

--- Exam Preparation/10-Dec-2021/10-Dec-2021.sql
+++ Exam Preparation/10-Dec-2021/10-Dec-2021.sql
@@ -92,3 +92,30 @@ DELETE
 
 -- Problem 05
 
+  SELECT Manufacturer,
+         Model,
+         FlightHours,
+         Condition
+    FROM Aircraft
+ORDER BY FlightHours DESC
+
+
+-- Problem 06
+
+  SELECT p.FirstName,
+		 p.LastName,
+		 a.Manufacturer,
+		 a.Model,
+		 a.FlightHours
+	FROM Pilots AS p
+	JOIN PilotsAircraft AS pa
+	  ON p.Id = pa.PilotId
+	JOIN Aircraft AS a
+      ON pa.AircraftId = a.Id
+   WHERE a.FlightHours IS NOT NULL AND a.FlightHours <= 304
+ORDER BY a.FlightHours DESC,
+		 p.FirstName
+
+
+-- Problem 07
+

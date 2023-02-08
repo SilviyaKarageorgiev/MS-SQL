05e47953adfb980611d7edc8fdf853bfafe776a5
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-02-08 17:22:04 +0200

    Add

--- Exam Preparation/10-Dec-2021/10-Dec-2021.sql
+++ Exam Preparation/10-Dec-2021/10-Dec-2021.sql
@@ -137,3 +137,15 @@ ORDER BY fd.TicketPrice DESC,
 
 -- Problem 08
 
+  SELECT 
+		 a.Id AS AircraftId,
+		 a.Manufacturer,
+		 a.FlightHours,
+		 COUNT(a.Id) AS FlightDestinationsCount,
+		 ROUND(AVG(fd.TicketPrice), 2) AS AvgPrice
+	FROM Aircraft AS a
+    JOIN FlightDestinations AS fd
+      ON a.Id = fd.AircraftId
+GROUP BY a.Id, a.Manufacturer, a.FlightHours
+  HAVING COUNT(a.Id) >= 2
+ORDER BY COUNT(a.Id) DESC, a.Id

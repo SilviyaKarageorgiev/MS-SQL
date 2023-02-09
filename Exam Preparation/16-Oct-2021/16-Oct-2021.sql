fb08b4c42b66d86a8d6218488f5695a0de2b94d3
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-02-09 19:10:31 +0200

    Update 16-Oct-2021.sql

--- Exam Preparation/16-Oct-2021/16-Oct-2021.sql
+++ Exam Preparation/16-Oct-2021/16-Oct-2021.sql
@@ -88,15 +88,15 @@ VALUES
 -- Problem 03
 
 UPDATE Cigars
-SET PriceForSingleCigar += 0.2 * PriceForSingleCigar
-WHERE TastId = (
+   SET PriceForSingleCigar += 0.2 * PriceForSingleCigar
+ WHERE TastId = (
 	SELECT Id FROM Tastes
-	WHERE TasteType = 'Spicy'
+	 WHERE TasteType = 'Spicy'
 )
 
 UPDATE Brands
-SET BrandDescription = 'New description'
-WHERE BrandDescription IS NULL
+   SET BrandDescription = 'New description'
+ WHERE BrandDescription IS NULL
 
 
 -- Problem 04
@@ -104,7 +104,7 @@ WHERE BrandDescription IS NULL
 DELETE FROM Clients
 WHERE AddressId = (
 	SELECT Id FROM Addresses
-	WHERE Country LIKE 'C%'
+	 WHERE Country LIKE 'C%'
 )
 
 DELETE FROM Addresses
@@ -114,20 +114,20 @@ WHERE Country LIKE 'C%'
 -- Problem 05
 
   SELECT CigarName,
-		 PriceForSingleCigar,
-		 ImageURL
-	FROM Cigars
+	 PriceForSingleCigar,
+	 ImageURL
+    FROM Cigars
 ORDER BY PriceForSingleCigar,
-		 CigarName DESC
+	 CigarName DESC
 
 
 -- Problem 06
 
   SELECT c.Id,
-	     c.CigarName,
-	     c.PriceForSingleCigar,
-	     t.TasteType,
-	     t.TasteStrength
+	 c.CigarName,
+	 c.PriceForSingleCigar,
+	 t.TasteType,
+	 t.TasteStrength
     FROM Cigars AS c
     JOIN Tastes AS t ON c.TastId = t.Id
    WHERE t.TasteType IN ('Earthy', 'Woody')
@@ -137,8 +137,8 @@ ORDER BY c.PriceForSingleCigar DESC
 -- Problem 07
 
    SELECT c.Id,
-	      CONCAT(c.FirstName, ' ', c.LastName) AS ClientName,
-		  c.Email
+	  CONCAT(c.FirstName, ' ', c.LastName) AS ClientName,
+	  c.Email
      FROM Clients AS c
 LEFT JOIN ClientsCigars AS cc ON c.Id = cc.ClientId
     WHERE cc.CigarId IS NULL
@@ -148,28 +148,28 @@ LEFT JOIN ClientsCigars AS cc ON c.Id = cc.ClientId
 -- Problem 08
 
   SELECT TOP(5)
-		 c.CigarName,
-		 c.PriceForSingleCigar,
-		 c.ImageURL
-	FROM Cigars AS c
+	 c.CigarName,
+	 c.PriceForSingleCigar,
+	 c.ImageURL
+    FROM Cigars AS c
     JOIN Sizes AS s ON c.SizeId = s.Id
    WHERE s.[Length] >= 12 AND (c.CigarName LIKE '%ci%' 
-		 OR c.PriceForSingleCigar > 50) AND s.RingRange > 2.55
+	 OR c.PriceForSingleCigar > 50) AND s.RingRange > 2.55
 ORDER BY c.CigarName,
-		 c.PriceForSingleCigar DESC
+	 c.PriceForSingleCigar DESC
 
 
 -- Problem 09
 
   SELECT 
-		 CONCAT(c.FirstName, ' ', c.LastName) AS FullName,
-		 a.Country,
-		 a.ZIP,
-	     CONCAT('$', MAX(cg.PriceForSingleCigar)) AS CigarPrice
-	FROM Addresses AS a
-	JOIN Clients AS c ON a.Id = c.AddressId
-	JOIN ClientsCigars AS cc ON cc.ClientId = c.Id
-	JOIN Cigars AS cg ON cc.CigarId = cg.Id
+         CONCAT(c.FirstName, ' ', c.LastName) AS FullName,
+         a.Country,
+         a.ZIP,
+         CONCAT('$', MAX(cg.PriceForSingleCigar)) AS CigarPrice
+    FROM Addresses AS a
+    JOIN Clients AS c ON a.Id = c.AddressId
+    JOIN ClientsCigars AS cc ON cc.ClientId = c.Id
+    JOIN Cigars AS cg ON cc.CigarId = cg.Id
    WHERE a.ZIP NOT LIKE '%[^0-9]%'
 GROUP BY c.Id, c.FirstName, c.LastName, a.Country, a.ZIP
 ORDER BY FullName
@@ -221,19 +221,19 @@ CREATE PROCEDURE usp_SearchByTaste(@taste VARCHAR(20))
 AS
 BEGIN
 	SELECT 
-c.CigarName,
-CONCAT('$', c.PriceForSingleCigar) AS Price,
-t.TasteType,
-b.BrandName,
-CONCAT(s.[Length], ' ', 'cm') AS CigarLength,
-CONCAT(s.RingRange, ' ', 'cm') AS CigarRingRange
-FROM Cigars AS c
-JOIN Brands AS b ON c.BrandId = b.Id
-JOIN Sizes AS s ON c.SizeId = s.Id
-JOIN Tastes AS t ON c.TastId = t.Id
-WHERE t.TasteType = @taste
-ORDER BY s.[Length],
-s.RingRange DESC
+		c.CigarName,
+		CONCAT('$', c.PriceForSingleCigar) AS Price,
+		t.TasteType,
+		b.BrandName,
+		CONCAT(s.[Length], ' ', 'cm') AS CigarLength,
+		CONCAT(s.RingRange, ' ', 'cm') AS CigarRingRange
+	   FROM Cigars AS c
+	   JOIN Brands AS b ON c.BrandId = b.Id
+	   JOIN Sizes AS s ON c.SizeId = s.Id
+	   JOIN Tastes AS t ON c.TastId = t.Id
+	  WHERE t.TasteType = @taste
+       ORDER BY s.[Length],
+		s.RingRange DESC
 END
 
 GO

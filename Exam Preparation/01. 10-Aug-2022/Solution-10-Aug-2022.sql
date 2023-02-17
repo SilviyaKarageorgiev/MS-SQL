f1f4367d6df2207eb55db318d491bd5196e39d20
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-02-17 23:18:19 +0200

    Update Solution-10-Aug-2022.sql

--- Exam Preparation/01. 10-Aug-2022/Solution-10-Aug-2022.sql
+++ Exam Preparation/01. 10-Aug-2022/Solution-10-Aug-2022.sql
@@ -5,49 +5,49 @@ USE NationalTouristSitesOfBulgaria
 -- Problem 01
 
 CREATE TABLE Tourists (
-			 Id INT PRIMARY KEY IDENTITY,
-			 [Name] VARCHAR(50) NOT NULL,
-			 Age INT NOT NULL,
-			 PhoneNumber VARCHAR(20) NOT NULL,
-			 Nationality VARCHAR(30) NOT NULL,
-			 Reward VARCHAR(20)
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	Age INT NOT NULL,
+	PhoneNumber VARCHAR(20) NOT NULL,
+	Nationality VARCHAR(30) NOT NULL,
+	Reward VARCHAR(20)
 )
 
 CREATE TABLE BonusPrizes (
-			 Id INT PRIMARY KEY IDENTITY,
-			 [Name] VARCHAR(50) NOT NULL
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL
 )
 
 CREATE TABLE TouristsBonusPrizes (
-			 TouristId INT FOREIGN KEY REFERENCES Tourists(Id) NOT NULL,
-			 BonusPrizeId INT FOREIGN KEY REFERENCES BonusPrizes(Id) NOT NULL,
-			 PRIMARY KEY (TouristId, BonusPrizeId)
+	TouristId INT FOREIGN KEY REFERENCES Tourists(Id) NOT NULL,
+	BonusPrizeId INT FOREIGN KEY REFERENCES BonusPrizes(Id) NOT NULL,
+	PRIMARY KEY (TouristId, BonusPrizeId)
 )
 
 CREATE TABLE Locations (
-			 Id INT PRIMARY KEY IDENTITY,
-			 [Name] VARCHAR(50) NOT NULL,
-			 Municipality VARCHAR(50),
-			 Province VARCHAR(50)
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	Municipality VARCHAR(50),
+	Province VARCHAR(50)
 )
 
 CREATE TABLE Categories (
-			 Id INT PRIMARY KEY IDENTITY,
-			 [Name] VARCHAR(50) NOT NULL
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL
 )
 
 CREATE TABLE Sites (
-			 Id INT PRIMARY KEY IDENTITY,
-			 [Name] VARCHAR(100) NOT NULL,
-			 LocationId INT FOREIGN KEY REFERENCES Locations(Id) NOT NULL,
-			 CategoryId INT FOREIGN KEY REFERENCES Categories(Id) NOT NULL,
-			 Establishment VARCHAR(15)
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(100) NOT NULL,
+	LocationId INT FOREIGN KEY REFERENCES Locations(Id) NOT NULL,
+	CategoryId INT FOREIGN KEY REFERENCES Categories(Id) NOT NULL,
+	Establishment VARCHAR(15)
 )
 
 CREATE TABLE SitesTourists (
-			 TouristId INT FOREIGN KEY REFERENCES Tourists(Id) NOT NULL,
-			 SiteId INT FOREIGN KEY REFERENCES Sites(Id) NOT NULL,
-			 PRIMARY KEY (TouristId, SiteId)
+	TouristId INT FOREIGN KEY REFERENCES Tourists(Id) NOT NULL,
+	SiteId INT FOREIGN KEY REFERENCES Sites(Id) NOT NULL,
+	PRIMARY KEY (TouristId, SiteId)
 )
 
 
@@ -55,19 +55,19 @@ CREATE TABLE SitesTourists (
 
 INSERT INTO Tourists([Name], Age, PhoneNumber, Nationality, Reward)
 	 VALUES
-			('Borislava Kazakova', 52, '+359896354244', 'Bulgaria', NULL),
-			('Peter Bosh', 48, '+447911844141', 'UK', NULL),
-			('Martin Smith', 29, '+353863818592', 'Ireland', 'Bronze badge'),
-			('Svilen Dobrev', 49, '+359986584786', 'Bulgaria', 'Silver badge'),
-			('Kremena Popova', 38, '+359893298604', 'Bulgaria', NULL)
+	('Borislava Kazakova', 52, '+359896354244', 'Bulgaria', NULL),
+	('Peter Bosh', 48, '+447911844141', 'UK', NULL),
+	('Martin Smith', 29, '+353863818592', 'Ireland', 'Bronze badge'),
+	('Svilen Dobrev', 49, '+359986584786', 'Bulgaria', 'Silver badge'),
+	('Kremena Popova', 38, '+359893298604', 'Bulgaria', NULL)
 
 INSERT INTO Sites([Name], LocationId, CategoryId, Establishment)
 	 VALUES
-			('Ustra fortress', 90, 7, 'X'),
-			('Karlanovo Pyramids', 65, 7, NULL),
-			('The Tomb of Tsar Sevt', 63, 8, 'V BC'),
-			('Sinite Kamani Natural Park', 17, 1, NULL),
-			('St. Petka of Bulgaria – Rupite', 92, 6, '1994')
+	('Ustra fortress', 90, 7, 'X'),
+	('Karlanovo Pyramids', 65, 7, NULL),
+	('The Tomb of Tsar Sevt', 63, 8, 'V BC'),
+	('Sinite Kamani Natural Park', 17, 1, NULL),
+	('St. Petka of Bulgaria – Rupite', 92, 6, '1994')
 
 
 -- Problem 03 
